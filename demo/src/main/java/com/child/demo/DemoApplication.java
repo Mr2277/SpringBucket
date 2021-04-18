5db6ac788777d70d69a456221e97f0a756c8d242
sun <837544713@qq.com>
2021-04-18 16:31:59 +0800

    ceshi

--- demo/src/main/java/com/child/demo/DemoApplication.java
+++ demo/src/main/java/com/child/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("新的电脑");
         System.out.println("测试Git");
+        System.out.println("ceshi");
         SpringApplication.run(DemoApplication.class, args);
     }
 
